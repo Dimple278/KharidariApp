96797fbbcf50e6fd7f3252b87fb48de09e9dd333
Dimple <dsaraogi2000@gmail.com>
2024-03-22 14:09:30 +0545

    added navbar and footer

---
+++ src/app/accessories/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const accessories = () => {
+  return <div>Accessories page</div>;
+};
+
+export default accessories;
---
+++ src/app/tshirts/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const tshirts = () => {
+  return <div>tshirts page</div>;
+};
+
+export default tshirts;
